f98364cc423af2a9457c6b2dfd10b243932ec25c
weiya <weix0015@stud.kea.dk>
2023-09-16 21:49:01 +0200

    commit init

--- index.js
+++ index.js
@@ -1,196 +1,200 @@
 let kommuner = [];
 
-const URLkommuner = "http://localhost:8080/getkommuner";
-const URLkommune  = `http://localhost:8080/kommune/${region}`;
-const URLRegioner = "http://localhost:8080/getregioner";
+const URLkommuner = "http://localhost:8080/kommuner"
+const URLkommune  = "http://localhost:8080/kommune"
+const URLregioner  = "http://localhost:8080/regioner"
+
+const kommuneTableBody = document.getElementById("kommune-table-body")
+// bootstrap modal
+let kommuneModal
+const modalTitle = document.getElementById("modal-title")
+const buttonSave = document.getElementById("btn-save")
+const buttonAdd = document.getElementById("btn-add-kommune")
+// form
+const kommuneForm = document.getElementById("kommuneForm")
+const inputKode = document.getElementById("input-kode")
+const inputNavn = document.getElementById("input-navn")
+const inputRegionSelect = document.getElementById("input-region")
+
+async function fetchAndParse(url, options) {
+  const result = await fetch(url, options)
+  if ( !result.ok ) throw new Error( await result.text() )
+
+  return await result.json()
+}
 
 //Handlers
 function setUpHandlers() {
-  document.getElementById("kommune-table-body").onclick = handleTableClick
-  document.getElementById("btn-save").onclick = saveKommune
-  document.getElementById("btn-add-kommune").onclick = makeNewKommune
+  kommuneTableBody.addEventListener( 'click', editOrDeleteTableItem )
+  buttonSave.addEventListener( 'click', event => validateForm(event, true))
+  buttonAdd.addEventListener( 'click', () => showModal() )
 }
-setUpHandlers()
 
-//Regioner
-async function fetchRegioner() {
-  try {
-    regioner = await fetch(URLRegioner)
-        .then(handleHttpErrors)
-    console.log(regioner)
-  } catch (err) {
-    if (err.apiError) {
-      console.error("Full API error: ", err.apiError)
-    }else {
-      console.error(err.message)
-    }
+function validateInput() {
+  const inputRegionInput = inputRegionSelect.selectedOptions
+  if (!inputKode.value) return false
+  if (!inputNavn.value) return false
+  if (!inputRegionInput?.[0].value) return false
+  return true
+}
+
+function validateForm(event, shouldSubmit = true) {
+  const inputKodeErrorMessage = document.querySelector( '#input-kode ~ .invalid-feedback' )
+  inputKodeErrorMessage.innerText = 'Please provide valid kode.'
+  inputKode.setCustomValidity('')
+  if (!inputKode.disabled && getKommuneIndexByKode(inputKode.value) > -1) {
+    inputKodeErrorMessage.innerText = 'Duplicated kode.'
+    inputKode.setCustomValidity('duplicated')
+    event.stopPropagation()
+    event.preventDefault()
+  } else if (!validateInput()) {
+    event.stopPropagation()
+    event.preventDefault()
+  } else if (!shouldSubmit) {
+  } else {
+    kommuneModal.hide()
+    addOrEditKommune()
   }
-
-  makeRows()
+  kommuneForm.classList.add('was-validated')
 }
+
 //Table
-function makeRows() {
+function renderRows() {
   //make rows from data
-  const rows = kommuner.map(k => `
+  const rows = kommuner.map(kommune => `
         <tr>
-            <td>${k.kode}</td>
-            <td>${k.navn}</td>
-            <td>${k.region.navn}</td>
-            <td><a data-id-delete=${k.id} href="#">Delete</a></td>
-            <!-- <td><a data-data-edit='${JSON.stringify(k)}' href="#">Edit</a></td> -->
-            <td><a data-id-edit='${k.id}' href="#">Edit</a></td>
+            <td>${kommune.kode}</td>
+            <td>${kommune.navn}</td>
+            <td>${kommune.region.navn}</td>
+            <td><a data-id-delete=${kommune.kode} href="#">Delete</a></td>
+            <td><a data-id-edit="${kommune.kode}" href="#">Edit</a></td>
         </tr>
         `)
-  document.getElementById("kommune-table-body").innerHTML = rows.join("")
-}
-//Kommuner
-fetchRegioner()
-fetchKommuner()
-async function fetchKommuner() {
-
-  try {
-    kommuner = await fetch(URLkommuner)
-        .then(handleHttpErrors)
-    console.log(kommuner)
-  } catch (err) {
-    if (err.apiError) {
-      console.error("Full API error: ", err.apiError)
-    }else {
-      console.error(err.message)
-    }
-  }
-
-  makeRows()
-}
-//Edit & Delete
-function handleTableClick(evt) {
-  evt.preventDefault()
-  evt.stopPropagation()
-  const target = evt.target;
-
-  if (target.dataset.idDelete) {
-    const idToDelete = Number(target.dataset.idDelete)
-
-    const options = makeOptions("DELETE")
-    fetch(`${URLkommune}/${idToDelete}`, options)
-        .then(handleHttpErrors)
-        .catch(err => {
-          if (err.apiError){
-            console.error("Full API error: ", err.apiError)
-
-          }else{
-            console.error(err.message)
-          }
-        })
-    kommuner = kommuner.filter(k => k.id !== idToDelete)
-
-    makeRows()
-  }
-
-  if (target.dataset.idEdit){
-    const idToEdit = Number(target.dataset.idEdit)
-    kommuner = kommuner.find(k => k.id === idToEdit)
-    showModal(kommuner)
-  }
+  kommuneTableBody.innerHTML = rows.join("\n")
 }
 
-//Error
-
-async function handleHttpErrors(res) {
-  if (!res.ok) {
-    const errorResponse = await res.json();
-    const error = new Error(errorResponse.message)
-    error.apiError = errorResponse
-    throw error
-  }
-  return res.json()
-}
 //Modal
-
 function showModal(kommune) {
-  const myModal = new bootstrap.Modal(document.getElementById('kommune-modal'))
-  document.getElementById("modal-title").innerText = kommune.kode ? "Edit Kommuner" : "Add Kommuner"
-  document.getElementById("kommuneKode").innerText = kommune.kode
-  document.getElementById("kommune-navn").value = kommune.name
+  kommuneModal = new bootstrap.Modal(document.getElementById('kommune-modal'))
+  const isEdit = Boolean( kommune?.kode )
+  const inputRegionOption = inputRegionSelect.querySelector("option")
 
-  myModal.show()
-}
-
-async function saveKommune() {
-  let kommune = {}
-  kommune.id = Number(document.getElementById("kommune-id").innerText)
-  student.name = document.getElementById("input-nave").value
-  student.region = document.getElementById("input-region").value
-
-  if (kommune.id){ //Edit
-    //apiStudentPut(student)
-    const options = makeOptions("PUT", kommune)
-    try {
-      kommune = await fetch(`${URLkommune}/${kommune.id}`, options)
-          .then(handleHttpErrors)
-    } catch (err) {
-      if (err.apiError){
-        console.error("Full API error: ", err.apiError)
-      } else {
-        console.error(err.message)
-      }
-    }
-    kommuner = kommuner.map(k => (k.id === kommune.id) ? kommune : k)
+  if ( isEdit ) {
+    modalTitle.innerText = "Edit Kommune"
+    inputKode.disabled = true
+    inputKode.value = kommune?.kode || ''
+    inputNavn.value = kommune?.navn || ''
+    inputRegionSelect.disabled = true
+    inputRegionOption.value = kommune?.region?.kode
+    inputRegionOption.innerText = kommune?.region?.navn
+    inputRegionOption.selected = true
   } else {
-    const options = makeOptions("POST", kommune)
-    try {
-      kommune = await fetch(URLkommuner, options)
-          .then(handleHttpErrors)
-    } catch (err) {
-      if (err.apiError){
-        console.error("Full API error: ", err.apiError)
-      }   else {
-        console.error(err.message)
-      }
-
-    }
-    kommuner.push(kommune)
+    modalTitle.innerText = "Add Kommune"
+    inputKode.disabled = false
+    inputKode.value = kommune?.kode || ''
+    inputNavn.value = ''
+    inputRegionSelect.disabled = false
+    inputRegionOption.value = ''
+    inputRegionOption.innerText = 'Choose...'
+    inputRegionOption.selected = true
   }
 
-  makeRows()
+  kommuneModal.show()
 }
 
-
-//Kommune
-async function saveKommune() {
-  let kommuner = {}
-  kommuner.kode = Number(document.getElementById("input-kode").innerText)
-  kommuner.navn = document.getElementById("input-navn").value
-  kommuner.region = document.getElementById("input-region").value
-
-  if (kommuner.kode){
-    const options = makeOptions("PUT",kommuner)
-    try {
-      kommuner = await fetch(`${URLkommuner}/${kommuner.kode}`,options)
-          .then(handleHttpErrors)
-    }catch (err){
-      if (err.apiError){
-        console.error("Full API error: ", err.apiError)
-      }else {
-        console.error(err.message)
-      }
+function getKommuneIndexByKode( kode ) {
+  for ( let i = 0; i < kommuner.length; ++i ) {
+    if ( kommuner[i].kode === kode ) {
+      return i
     }
-    kommuner = kommuner.map(k => (k.kode === kommuner.kode) ? kommuner : k)
-  } else {
-    const options = makeOptions("POST",kommuner)
-    try {
-      kommuner = await fetch(URLkommuner,options)
-          .then(handleHttpErrors)
-
-    } catch (err){
-      if (err.apiError){
-        console.log("Full API error: ", err.apiError)
-      }else {
-        console.log(err.message)
-      }
-    }
-    kommuner.push(kommuner)
   }
-
-  makeRows()
+  return -1
 }
+
+// Kommune
+async function fetchKommuner() {
+  kommuner = await fetchAndParse(URLkommuner)
+  renderRows()
+}
+
+async function addOrEditKommune() {
+  const kode = inputKode.value
+  const regionOption = inputRegionSelect.selectedOptions[0]
+  const newKommune = {
+    kode: kode,
+    navn: inputNavn.value,
+    region: {
+      kode: regionOption.value,
+      navn: regionOption.innerText
+    }
+  }
+  if (!newKommune.region.kode) return
+
+  const kommuneIndex = getKommuneIndexByKode(kode)
+  if (kommuneIndex > -1) {
+    // Edit
+    await fetchAndParse(
+      `${URLkommune}/${kode}`,
+      {
+        method: "PATCH",
+        body: JSON.stringify(newKommune),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    )
+    kommuner[kommuneIndex] = newKommune
+  } else {
+    // Add
+    await fetchAndParse(
+      `${URLkommune}/${kode}`,
+      {
+        method: "POST",
+        body: JSON.stringify(newKommune),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    )
+    kommuner.push(newKommune)
+  }
+  renderRows()
+}
+
+async function deleteKommune(kode) {
+  await fetchAndParse(
+    `${URLkommune}/${kode}`,
+    {
+      method: "DELETE"
+    }
+  )
+  const kommuneIndex = getKommuneIndexByKode(kode)
+  if (kommuneIndex > -1) kommuner.splice(kommuneIndex, 1)
+  renderRows()
+}
+
+// Edit & Delete
+async function editOrDeleteTableItem(event) {
+  event.preventDefault()
+  event.stopPropagation()
+  const target = event.target
+  const idToDelete = target.getAttribute('data-id-delete')
+  const idToEdit = target.getAttribute('data-id-edit')
+
+  if (idToDelete) {
+    await deleteKommune(idToDelete)
+  } else if (idToEdit) {
+    const kommuneIndex = getKommuneIndexByKode(idToEdit)
+    if (kommuneIndex > -1) showModal(kommuner[kommuneIndex])
+  }
+}
+
+async function main() {
+  setUpHandlers()
+  await fetchAndParse( URLregioner )
+  await fetchKommuner()
+}
+
+(async () => {
+  await main()
+})()
